de535a244318d8bfcc5e1c9ca8c15052fcadc69a
Vu Hoang <vuhoang@Vus-MacBook-Air.local>
2025-01-21 14:30:22 +0700

    lock

--- src/main/java/org/example/BankAccount.java
+++ src/main/java/org/example/BankAccount.java
@@ -3,8 +3,18 @@ package org.example;
 public class BankAccount {
     private double balance;
 
+    private String name;
+
+    private final Object lockName = new Object();
+    private final Object lockBalance = new Object();
+
     public BankAccount(double balance) {
         this.balance = balance;
+    }
+
+    public BankAccount(String name, double balance) {
+        this.balance = balance;
+        this.name = name;
     }
 
     public double getBalance() {
@@ -15,6 +25,18 @@ public class BankAccount {
         this.balance = balance;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        synchronized(lockName) {
+            this.name = name;
+            System.out.println("Updated name to " + name);
+        }
+
+    }
+
     public void deposit(double amount) {
         try {
             System.out.println("Deposit taking to the teller at the bank: ...");
@@ -22,11 +44,12 @@ public class BankAccount {
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
-        synchronized (this) {
+        synchronized (lockBalance) {
             double originalBalance = balance;
             balance = balance + amount;
             System.out.println("Starting balance " + originalBalance + " deposit " + amount
                     + " New balance " + balance);
+            addPromoDolar(amount);
         }
 
     }
@@ -47,5 +70,12 @@ public class BankAccount {
                     "Insufficient balance");
         }
 
+    }
+
+    private void addPromoDolar(double amount) {
+        if (amount >= 5000) {
+            System.out.println("Congratulations! You have been promoted into the bank!");
+            balance += 25;
+        }
     }
 }
